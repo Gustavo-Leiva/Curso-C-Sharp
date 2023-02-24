a6d9057953ec785f5a7d217c34f84eedfd1727cf
Gustavo Leiva <leiva.gustavo@hotmail.com.ar>
2023-02-24 16:01:02 -0300

    Arquitecturas en capas

---
+++ Curso C# Nivel 2/7.Arquitecturas en Capas/Arquitectura-en-Capas/dominio/Elemento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arquitectura_en_Capas
+{
+    public class Elemento
+    {
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+
+    }
+}
